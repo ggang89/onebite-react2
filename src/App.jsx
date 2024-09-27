45ab0cc8460c8b3779e014c6d208ffdd40e09a14
ggang89 <ggang89@gmail.com>
2024-09-27 19:34:18 +0900

    값이 없는 props전달시 오류 방지 방법 1) Button.defaultProps로 기본값 설정 2) 옵셔널 체이닝 ? 사용 //전개 연산자로 props의 모든 값 전달하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import Button from "./components/Button";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
+
+
+const buttonProps = { text: "메일", color: "red" }
 
 function App() {
   return (
     <>
       <Button text={"메일"} color={"red"} />
+      <Button {...buttonProps} />
       <Button text={"카페"} />
       <Button text={"블로그"} />
     </>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,22 @@
-export default function Button(props) {
+export default function Button(props) { 
   console.log(props); //{text:"카페"} 객체형태를 가짐
   return (
-    <button style={{ color: props.color }}>
-      {props.text} {props.color}
+    <button style={{ color: props.color }}> 
+      {props.text} {props.color?.toUpperCase()}
     </button>
   );
 }
 //const props ={text:"카페"}
 //const {text}=props
 //console.log(text) //카페
+
+//props.color가 없는 경우 toUpperCase메서드로 인해서 오류 발생함
+//undefined에 메서드를 호출하는 것이므로
+//오류 방지를 위한 방법
+// 1. "기본값"을 설정
+// Button.defaultProps = {
+//   color: "black"
+// };
+// 2. 옵셔널 체이닝 사용
+// props.color?.toUpperCase()
+//color가 없는 경우 undefined가 반환되므로 오류가 안 남!
