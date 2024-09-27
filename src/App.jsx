5b48e254921e14e2cc240e365962b64b1c6fc147
ggang89 <ggang89@gmail.com>
2024-09-27 19:41:32 +0900

    children props로 컴포넌트 안에 자식요소 전달하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import "./App.css";
 import Button from "./components/Button";
-
-
-const buttonProps = { text: "메일", color: "red" }
+import Header from "./components/Header";
 
 function App() {
+  const buttonProps = { text: "메일", color: "red" };
   return (
     <>
       <Button text={"메일"} color={"red"} />
       <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"} />
+      <Button text={"카페"}>
+        <Header />
+      </Button>
+      <Button text={"블로그"}>
+        <div>자식요소 전달하기</div>
+      </Button>
     </>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,7 @@ export default function Button(props) {
   return (
     <button style={{ color: props.color }}> 
       {props.text} {props.color?.toUpperCase()}
+      {props.children}
     </button>
   );
 }
