c99a4f31729520307ebdca865ef2947c1a1bdd89
ggang89 <ggang89@gmail.com>
2024-09-27 23:03:04 +0900

    하나의 앱에서 컴포넌트끼리 묶기 => 부모가 렌더링 되면서 자식까지 불필요한 렌더링 되는 것을 방지하기위해

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,51 @@
 import { useState } from "react";
 
-const Bulb = ({ light }) => {
-  return (
-    <div>
-      {light === "on" ? (
-      <h1 style={{color:'orange'}}>ON</h1>
-      ):(<h1 style={{color:"gray"}}>OFF</h1>)}
-    </div>
-  )
-}
-
-function App() {
-  const [count, setCount] = useState(0);
+const Bulb = () => {
   const [light, setLight] = useState("off");
-
   return (
     <>
       <div>
-        <h1><Bulb light={light } /></h1>
-        <button
-          onClick={() => {
-            setLight(light === "on" ? "off" : "on");
-          }}
-        >
-          {light === "off" ? "켜기" : "끄기"}
-        </button>
+        {light === "on" ? (
+          <h1 style={{ color: "orange" }}>ON</h1>
+        ) : (
+          <h1 style={{ color: "gray" }}>OFF</h1>
+        )}
       </div>
-      <div>
-        <h1>{count}</h1>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
+      <button
+        onClick={() => {
+          setLight(light === "on" ? "off" : "on");
+        }}
+      >
+        {light === "off" ? "켜기" : "끄기"}
+      </button>
+    </>
+  );
+}
+
+const Count = () => {
+  const [count, setCount] = useState(0);
+  console.log(count)
+  return (
+    <>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
+    </>
+  );
+}
+function App() {
+  
+
+  return (
+    <>
+      
+        <Bulb  />
+        <Count/>  
     </>
   );
 }
