7f782c664970fbef24386c5ccdd9eda7abf9ba27
ggang89 <ggang89@gmail.com>
2024-09-28 17:23:00 +0900

    useRef 사용하기

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 //간단한 회원가입 폼 만들기
 //1. 이름 2. 생년월일  3.국적  4.자기소개
@@ -11,22 +11,43 @@ export default function Register() {
     live: "",
     intro: "",
   });
+
+  const countRef = useRef(0); //{current:0}
+  const inputRef = useRef(); //DOM요소 조작하기
+
   const onChange = (e) => {
+    countRef.current++;
+    console.log(countRef.current);
     setInput({
       ...input,
-      [e.target.name]:e.target.value,
-    })
-   };
-  
-  
-  
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = () => {
+    if (input.name === "") {
+      //이름을 입력하는 DOM 요소 포커스하기
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <>
       <div>
-        <input name="name" value={input.name} onChange={onChange} />
+        <input
+          ref={inputRef}
+          name="name"
+          value={input.name}
+          onChange={onChange}
+        />
       </div>
       <div>
-        <input name="birth" type="date" value={input.birth} onChange={onChange} />
+        <input
+          name="birth"
+          type="date"
+          value={input.birth}
+          onChange={onChange}
+        />
       </div>
       <div>
         <select name="live" value={input.live} onChange={onChange}>
@@ -38,6 +59,8 @@ export default function Register() {
       <div>
         <textarea name="intro" value={input.intro} onChange={onChange} />
       </div>
+
+      <button onClick={onSubmit}>submit</button>
     </>
   );
 }
